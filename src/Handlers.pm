c9d1ba7e95c1a659dee363e17636570613402853
Alex Kapranoff <alex@kapranoff.ru>
2015-10-14 01:33:48 +0300

    Support attachments
    Async hell :(

--- src/Handlers.pm
+++ src/Handlers.pm
@@ -1,5 +1,7 @@
 package Handlers;
 use utf8;
+use strict;
+use warnings;
 
 use Exporter 'import';
 
@@ -7,6 +9,7 @@ our @EXPORT = qw/handle_bot_update/;
 
 use Mojo::JSON qw/encode_json decode_json/;
 use Mojo::Util qw/url_escape/;
+use Mojo::IOLoop;
 
 # states;
 # logged_off, logged_in
@@ -122,6 +125,141 @@ sub state_login_have_user {
 	}
 }
 
+sub upload_attachment {
+	my ($c, $link, $get_file, $file_name, $cb) = @_;
+	my $att_id;
+
+	my $get_url = 'https://api.telegram.org/file/bot'
+		. $c->config->{telegram_bot_token}
+		. "/$get_file->{file_path}";
+
+	$c->app->log->debug("[files] downloading: " . $get_url);
+
+	my $get_tx = $c->ua->get($get_url);
+	if ($get_tx->success) {
+		$c->app->log->debug("[files] uploading");
+		my $post_tx = $c->ua->post('https://freefeed.net/v1/attachments'
+			=> { 'X-Authentication-Token' => $link->{token} }
+			=> form
+			=>
+			{
+				name => 'attachment[file]',
+				'attachment[file]' => {
+					file	=> $get_tx->res->content->asset,
+					($file_name ? (filename => $file_name) : ()),
+					# mimetype
+				},
+			}
+		);
+		if ($post_tx->success) {
+			$c->app->log->debug("[files] successful upload: " . $post_tx->res->to_string);
+			$c->app->log->debug("[files] successful upload req was: " . $post_tx->req->to_string);
+			$att_id = $post_tx->res->json->{attachments}->{id};
+		}
+		else {
+			$c->app->log->debug("[files] uploading FAILED: " .  $post_tx->res->to_string);
+		}
+	}
+	else {
+		$c->app->log->debug("[files] download FAILED: " . $get_tx->res->to_string);
+	}
+	$cb->($att_id);
+}
+
+sub prepare_files {
+	my ($c, $message, $link, $cb) = @_;
+
+	my @rv;
+	my @files;
+
+	if (my $photos = $message->{photo}) {
+		push @files, [$photos->[-1]->{file_id}, undef];
+	}
+
+	foreach my $type (qw/voice audio sticker document video/) {
+		if (my $file = $message->{$type}) {
+			push @files, [$file->{file_id}, $file->{file_name}];
+		}
+	}
+
+	unless (@files) {
+		$cb->();
+		return;
+	};
+
+	$c->app->log->debug("[files] found files: " . encode_json(\@files));
+
+	Mojo::IOLoop->delay(
+		sub {
+			my $delay = shift;
+			foreach my $file (@files) {
+				my $end = $delay->begin(0);
+
+				$c->botapi->getFile({
+					file_id => $file->[0],
+				}, sub {
+					my ($ua, $tx) = @_;
+
+					if ($tx->success && $tx->res->json->{ok}) {
+						upload_attachment($c, $link, $tx->res->json->{result}, $file->[1], $end);
+					}
+					else {
+						$c->app->log->debug("[files] getFile error: " .  $tx->res->to_string);
+						$end->();
+					}
+				});
+			}
+		},
+		sub {
+			my $delay = shift;
+			$c->app->log->debug("[files] rv ready, calling cb: " . encode_json(\@_));
+			$cb->(@_);
+		}
+	);
+}
+
+sub conjure_text {
+	my $message = shift;
+
+	my $file;
+	if ($message->{caption}) {
+		return $message->{caption};
+	}
+
+	if ($message->{photo}) {
+		$file = $message->{photo}->[-1];
+		return 'image ' . $file->{width} . 'x' . $file->{height} . ($file->{file_size} ? " $file->{file_size} bytes" : '');
+	}
+
+	if ($message->{video}) {
+		$file = $message->{video};
+		return 'video ' . $file->{width} . 'x' . $file->{height} . ' ' . $file->{duration} . 's';
+	}
+
+	if ($message->{voice}) {
+		$file = $file->{voice};
+		return 'voice ' . $file->{duration} . 's';
+	}
+
+	if ($message->{audio}) {
+		$file = $file->{audio};
+		return ($file->{performer} ? "$file->{performer} - " : '')
+			. ($file->{title} // 'Audio Track')
+			. ' ' . $file->{duration} . 's'
+			. ($file->{file_size} ? " $file->{file_size} bytes" : '');
+	}
+
+	if ($message->{sticker}) {
+		$file = $message->{sticker};
+		return 'sticker ' . $file->{width} . 'x' . $file->{height} . ($file->{file_size} ? " $file->{file_size} bytes" : '');
+	}
+
+	if ($message->{document}) {
+		$file = $message->{document};
+		return ($file->{file_name} // $file->{mime_type} // 'document') . ($file->{file_size} ? " $file->{file_size} bytes" : '');
+	}
+}
+
 sub state_logged_in {
 	my ($c, $message, $link) = @_;
 
@@ -151,46 +289,66 @@ sub state_logged_in {
 		say_simple($c, $chat_id, "Очень длинно, не надо так.");
 	}
 	else {
-		my $tx = $c->ua->post('https://freefeed.net/v1/posts'
-			=> { 'X-Authentication-Token' => $link->{token} }
-			=> json
-			=> {
-				post => { body	=> $text },
-				meta => { feeds => $link->{to} || $link->{user} },
-			}
-			=> sub {
-				my ($ua, $tx) = @_;
-				if ($tx->res->code == 200 && (my $post_id = $tx->res->json->{posts}->{id})) {
-					my $url_group = $link->{user};
-					if ($link->{to}) {
-						$url_group = $link->{to}->[0];
-						delete $link->{to};
-						$c->redis->set($chat_id, encode_json($link));
-					}
+		Mojo::IOLoop->delay(
+			sub {
+				my $delay = shift;
+				prepare_files($c, $message, $link, $delay->begin(0));
+			},
+			sub {
+				my $delay = shift;
 
-					$c->botapi->sendMessage({
-						chat_id		=> $chat_id,
-						reply_to_message_id => $message->{message_id},
-						text		=> "Готово, смотрите в https://m.freefeed.net/$url_group/$post_id",
-					},
-					sub {
+				my @attachments = @_;
+				$c->app->log->debug("[files] files prepared: " . encode_json(\@attachments));
+
+				$text //= conjure_text($message);
+
+				my $tx = $c->ua->post('https://freefeed.net/v1/posts'
+					=> { 'X-Authentication-Token' => $link->{token} }
+					=> json
+					=> {
+						post => {
+							body => $text,
+							(@attachments
+								? ( attachments => \@attachments )
+								: ()
+							),
+						},
+						meta => { feeds => $link->{to} || $link->{user} },
+					}
+					=> sub {
 						my ($ua, $tx) = @_;
-						$c->app->log->debug("[handle] sendMSG w/ reply success");
-					});
-					botan_report($c, $message, 'message');
-				}
-				else {
-					$c->botapi->sendMessage({
-						chat_id		=> $chat_id,
-						reply_to_message_id => $message->{message_id},
-						text		=> "Что-то пошло не так, я не смог это запостить",
+						if ($tx->res->code == 200 && (my $post_id = $tx->res->json->{posts}->{id})) {
+							my $url_group = $link->{user};
+							if ($link->{to}) {
+								$url_group = $link->{to}->[0];
+								delete $link->{to};
+								$c->redis->set($chat_id, encode_json($link));
+							}
+
+							$c->botapi->sendMessage({
+								chat_id		=> $chat_id,
+								reply_to_message_id => $message->{message_id},
+								text		=> "Готово, смотрите в https://m.freefeed.net/$url_group/$post_id",
+							},
+							sub {
+								my ($ua, $tx) = @_;
+								$c->app->log->debug("[handle] sendMSG w/ reply success");
+							});
+							botan_report($c, $message, 'message');
+						}
+						else {
+							$c->app->log->debug("[handle] post error: " . $tx->res->to_string);
+							$c->app->log->debug("[handle] req was: " . $tx->req->to_string);
+							botan_report($c, $message, 'message_error');
+
+							$c->botapi->sendMessage({
+								chat_id		=> $chat_id,
+								reply_to_message_id => $message->{message_id},
+								text		=> "Что-то пошло не так, я не смог это запостить",
+							}, sub { });
+						}
 					},
-					sub {
-						my ($ua, $tx) = @_;
-						$c->app->log->debug("[handle] sendMSG w/ reply ERROR: " . $tx->res->to_string);
-					});
-					botan_report($c, $message, 'message_error');
-				}
+				);
 			},
 		);
 	}
